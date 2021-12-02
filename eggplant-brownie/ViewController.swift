476287914378b96f49da2492a5606821d1f3a8ee
Eduardo Frederico <eduardo.frederico@icloud.com>
2021-12-02 01:31:50 -0300

    04 - Conectando a interface ao código e o IBAction

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func enviar(_ sender: Any) {
+        print("alimento: --")
     }
-
-
 }
 
